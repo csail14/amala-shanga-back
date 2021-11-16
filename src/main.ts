6e794be6b6bbf53d29cd643b7bc4964eaf3e7eed
Camille <cssaillard@gmail.com>
2021-11-16 13:21:11 +0100

    last

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8000;
   const host = process.env.HOST || '0.0.0.0';
+  console.log('port', port);
   await app.listen(port, host, () => {
     console.log('Server started on ' + host + ':' + port);
   });
