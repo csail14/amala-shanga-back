d2948e5409b2a80b03f67f03ae2adee276919987
Camille <cssaillard@gmail.com>
2021-11-16 13:28:08 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('ici');
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8000;
   const host = process.env.HOST || '0.0.0.0';
